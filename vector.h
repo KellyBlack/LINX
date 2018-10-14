530ff23b50787d8f4df03dd2bd452bcbff796dee
KellyBlack <kjblack@gmail.com>
2018-10-14 15:20:58 -0400

    Added a comment header to the vector.h file to add a license and a description of the file.

--- vector.h
+++ vector.h
@@ -1,3 +1,40 @@
+/* ************************************************************************************************
+ * Author: Kelly Black
+ * Date: October 2018
+ *
+ * Program to calculate the relative weights associated with each flow in a tropic system
+ * with respect to the relative importance. Formula developed with Caner Kazanci, Malcolm
+ * Adams, Stuart Whipple, Aladeen Al Basheer, and Bernie Patton.
+ *
+ * This is a class that is used to keep track of any information organized as either a
+ * vector or an array. The SquareMatrix class is used to manipulate the square matrix
+ * composed of the columns under consideration. It uses LAPACK to determine if the
+ * matrix is of full rank and to approximate the condition number of the matrix.
+ *
+ * The order of the vectors matters, and this will differentiate the columns checked
+ * by the order they are given. If you do not want it to consider order, then you should
+ * give it the set of vectors in a sorted order.
+ *
+ * Copyright © 2018 Kelly Black
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * ************************************************************************************************ */
 #ifndef VECTOR_H
 #define VECTOR_H
 
